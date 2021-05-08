6862fdc949f0608f9acd311310ec105ec8393472
eric373 <ericryanco@gmail.com>
2021-05-08 14:59:10 -0400

    Added inspection of datatypes, class sizes, statistical summary, correlation, and skew

--- templates/eda_inspection.py
+++ templates/eda_inspection.py
@@ -1,6 +1,7 @@
 #exec(open('templates\\eda_inspection.py').read())
 import subprocess as sp
 import pandas as pd
+import numpy as np
 
 if __name__ == '__main__':
     sp.call('cls', shell = True)
@@ -17,15 +18,39 @@ if __name__ == '__main__':
     print('Number of Rows: {0}'.format(df.shape[0]))
     print('Number of Columns: {0}'.format(df.shape[1]))
     print('')
+
     print('Column Names:')
     for col in df.columns:
         print('    ' + col)
     print('')
+
     print('First 20 rows:')
     print(df.head(20))
     print('')
+
     print('Last 20 rows:')
     print(df.tail(20))
     print('')
+
+    print('Data types:')
+    datatypes = df.dtypes
+    for idx in datatypes.index:
+        print('    {0} - {1}'.format(idx, datatypes[idx]))
+    print('')
+
+    print('Class sizes:')
+    print(df.groupby(['class']).size())
+    print('')
+
     print('Statistical Summary:')
     print(df.describe())
+    print('')
+
+    print('Correlations between variables:')
+    print(df.corr())
+    print('')
+
+    # numbers closer to 0 mean the distribution is closer to Gaussian
+    print('Skew of variables:')
+    print(df.skew())
+    print('')
